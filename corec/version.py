6581371714e5371e5e5aec7965d768de9e836f8e
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-09-27 16:04:31 -0700

    Initial release

---
+++ corec/version.py
@@ -0,0 +1,12 @@
+
+VERSION='0.1'
+RELEASE='0.1.0'
+URL='https://github.com/vulogov/core.CLIPS'
+AUTHOR='Vladimir Ulogov'
+AUTHOR_EMAIL='vladimir.ulogov@me.com'
+READ_me = """"""
+
+def nsVersion(ns):
+    return VERSION
+def nsRelease(ns):
+    return RELEASE
